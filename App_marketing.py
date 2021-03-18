48a81d3d7b379ae82d30c9942c3f8fb19c53b481
Atlaoui Tarik <atlaouitarik@yahoo.fr>
2021-03-18 15:36:17 +0100

    plus que 4

--- App_marketing.py
+++ App_marketing.py
@@ -27,9 +27,23 @@ sncf_arret = pd.read_csv('Data/data_sncf/lignes_points_arret.csv', sep = ';')
 #print(sncf_equipements.iloc[:,1])
 
 # Selectionner la colonne 'gare' en utilisant loc
-
+#print(sncf_equipements.loc[0: , 'gare'])
 
 # Selectionner la 3ème et la 10ème ligne 
 # et les colonnes 'gare' et 'ascenceurs' avec iloc
+#print(sncf_equipements.iloc[[3,10],[1,8]])
 
-print(sncf_equipements.iloc[1, 8])
+# Selectionner de la 1ère à 
+# la 5ème colonne et les 3 dernières colonnes 
+# en utilisant loc
+# ici c'est surment faux mais ça ma gavé 
+#print(sncf_equipements.loc[0: , ['code_uic','cab_passage_elargi_controle', 'ascenceurs' ,'escaliers_mecaniques']])
+
+# Afficher uniquement la colonne "gare"
+#print(sncf_equipements['gare'])
+
+# Afficher les colonnes "gare" et "ascenceurs"
+#print(sncf_equipements.loc[0: , ['gare','ascenceurs']])
+
+# Remplacer les valeurs manquantes de sncf_equipements par -1
+#print(sncf_equipements.fillna(-1))
